e009dceb847871700192ff5f50cffd141e95c822
xsn34kzx <xsn34kzx@gmail.com>
2025-02-15 22:28:35 -0500

    Copy over Scaffolding

--- Program.cs
+++ Program.cs
@@ -1,10 +1,56 @@
-﻿namespace MoonWorksAssignmentPorts;
+﻿using System;
+using MoonWorks;
+using MoonWorks.Graphics;
 
-public class Program
+namespace MoonWorksAssignmentPorts;
+
+public class Program : Game
 {
-    public static void Main(string[] args)
+    //Assignment[] Assignments;
+    int AssignmentIndex = 0;
+
+    public Program(
+            AppInfo appInfo,
+            WindowCreateInfo windowCreateInfo,
+            FramePacingSettings framePacingSettings,
+            bool debugMode
+    ) : base(
+            appInfo,
+            windowCreateInfo,
+            framePacingSettings,
+            ShaderFormat.SPIRV | ShaderFormat.DXBC | ShaderFormat.DXIL | ShaderFormat.MSL,
+            debugMode
+        )
     {
-        Console.WriteLine("Hello, World!");
+        ShaderCross.Initialize();
+    }
+
+    override protected void Update(TimeSpan delta)
+    {
+    }
+
+    override protected void Draw(double alpha)
+    {
+    }
+
+    static void Main(string[] args)
+    {
+        var windowCreateInfo = new WindowCreateInfo(
+            "MoonWorksAssignmentPorts",
+            960,
+            720,
+            ScreenMode.Windowed
+        );
+        
+        var framePacingSettings = FramePacingSettings.CreateCapped(60, 120);
+
+        var game = new Program(
+                new AppInfo("", "MoonWorksAssignmentPorts"),
+                windowCreateInfo,
+                framePacingSettings,
+                false);
+
+        game.Run();
     }
 }
 
